bce7f71c628b5ada5de45d24fa5fd8662b531dfc
Bhaveshwagh04 <121219011+Bhaveshwagh04@users.noreply.github.com>
2024-05-05 13:29:57 +0530

    update code

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 import streamlit as st
-from src.helper import voice_input, llm_model_object,text_to_speech
+from src.helper import voice_input, llm_model_object, text_to_speech
 
 
 def main():
-    
+    st.title("Multilingual AI Assistant 🤖")
+
+    if st.button("Ask me anything"):
+        with st.spinner("Listening....."):
+            text = voice_input()
+            response = llm_model_object(text)
+            text(response)
+
+            audio_file = open("speech.mp3", "rb")
+            audio_byte = audio_file.read()
+
+            st.text_area()
+            st.audio()
+            st.download_button()
+
+
+if __name__ == "__main__":
+    main()
